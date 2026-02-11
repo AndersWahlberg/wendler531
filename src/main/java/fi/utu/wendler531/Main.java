758886d3301ae5c11900131ccd249d6532261866
AndersWahlberg <anwahlbe@gmail.com>
2026-02-11 20:30:12 +0200

    Initial Maven project structure

---
+++ src/main/java/fi/utu/wendler531/Main.java
@@ -0,0 +1,7 @@
+package fi.utu.wendler531;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Wendler 5/3/1 app starting...");
+    }
+}
---
+++ src/test/java/fi/utu/wendler531/MainTest.java
@@ -0,0 +1,11 @@
+package fi.utu.wendler531;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+    @Test
+    void smokeTest() {
+        assertTrue(true);
+    }
+}
